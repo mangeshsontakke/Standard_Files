4c16de9acf715b5173e1f7d72bcf3b865d2e61e5
Mangesh Sontakke <mangesh.sontake@gmail.com>
2020-05-05 09:03:02 +0530

    Standard Files

---
+++ TestConfig.java
@@ -0,0 +1,25 @@
+
+
+public class TestConfig{
+
+
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "vrp@gmail.com";
+	public static String password = "abcd";
+	public static String[] to ={"vaishalibakale@gmail.com"};
+	public static String subject = "Test Report";
+	
+	public static String messageBody ="TestMessage";
+	public static String attachmentPath=System.getProperty("user.dir")+"//Reports.zip";
+	public static String attachmentName="reports.zip";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
